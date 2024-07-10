49937b848e44f6cf5c312bd4a5877d61ace217bb
mo-jo-dev <b210055@nitsikkim.ac.in>
2024-07-10 16:31:15 +0530

    added the important middlewares for the project

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const {PORT} = require('./config/server_config');
-
+const responseTime = require('response-time');
 const apiRoutes = require('./routes/api_router');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(responseTime(function(req, res, time){
+    console.log(time + "ms");
+    res.setHeader('X-Response-Time', time);
+}));
+
+app.use(bodyParser.json());
+app.use(bodyParser.text());
+app.use(bodyParser.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 
 app.listen(PORT, () => {
--- src/routes/v1/ping_router.js
+++ src/routes/v1/ping_router.js
@@ -1,8 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
+
 const { pingV1 } = require('../../controllers/ping_controller');
 
-router.get('/', pingV1);
+function checkRequest(request, response, next){
+    console.log("Checking the request");
+    next();
+}
+
+function logger(request, response, next){
+    console.log("Logging Request");
+    next();
+}
+
+function authChecker(request, response, next){
+    console.log("Auth Checked");
+    next();
+}
+
+router.get('/', [checkRequest, logger, authChecker] , pingV1);
 
 module.exports = router;
